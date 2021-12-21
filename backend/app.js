c5146d61cd7578c7cd91bc925e51b1fd98b34b07
Fabien Detrait <fabiendetrait@yahoo.fr>
2021-12-21 23:22:50 +0100

    ajout backend (server) + (schéma de sauces) + MongoDB/CORS (app.js)

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');     // Besoin d'express
+const mongoose = require('mongoose');   // Importer mongosse
+const app = express();      // Création de notre application express
+
+// const userRoutes = require('./routes/user');    // Importer le routeur des utilisateurs
+
+require('dotenv').config();   // Sécuriser les données sensibles
+
+// Connecter notre API à notre base de données
+mongoose.connect(process.env.SECURE_MONGO,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Permettre toutes demandes de toutes origines d'accéder à notre API
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
+
+app.use(express.json());    // Extraire le corps JSON pour gérer requête POST
+
+
+
+
+
+
+
+
+// app.use('/api/auth', userRoutes);     // Enregistrer les routes
+
+module.exports = app;   // Exporter notre application
