395c56a95cb906e1d6cc2f3ab272ff3ea2a8d8bf
Fabien Detrait <fabiendetrait@yahoo.fr>
2022-01-04 22:26:00 +0100

    ajout des packages mongo-sanitize, rate-limit

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');   // Importer mongosse
 const path = require('path');   // Importer path
 const helmet = require('helmet'); // Importer helmet
 const app = express();      // Création de notre application express
+const rateLimit = require('express-rate-limit');  // Importer express-rate-limit
+const mongoSanitize = require('express-mongo-sanitize');  // Importer express-mongo-sanitize
 const saucesRoutes = require('./routes/sauces');  // Importer le router des sauces
 const userRoutes = require('./routes/user');  // Importer le router des users
+const { application } = require('express');
 
 require('dotenv').config();   // Sécuriser les données sensibles
 
@@ -23,8 +26,16 @@ app.use((req, res, next) => {
   next();
 });
 
+// Configuration du limiteur
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,  // 15 mins 
+  max: 100                   // 100 requêtes autorisées par fenêtre
+});
+
 app.use(express.json());    // Extraire le corps JSON / donne accès au corps de la requête
 app.use(helmet());  // Protéger l'application de vulnérabilités
+app.use(limiter);   // Limiter les requêtes
+app.use(mongoSanitize()); // Eliminer les entrées avec des caractères interdits dans MongoDB
 
 app.use('/images', express.static(path.join(__dirname, 'images'))); 
 
